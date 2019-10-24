cbc16dd408de2049ea486130b464f946d7ea9e4c
juney <juneyj114@gmail.com>
2019-10-25 07:47:46 +0900

    class working

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+};
+const genesisBlock = new Block(0, "1231412355", "", "Hello", 123456);
+let blockchain = [genesisBlock];
+const getBlockChain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+console.log(blockchain);
+//# sourceMappingURL=index.js.map
